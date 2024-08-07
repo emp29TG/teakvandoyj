b1758b75015df600da8bfacd05c9eb6c6f24b9aa
emp29TG <singinloginsingup@gmail.com>
2024-08-07 03:40:43 +0330

    Add files via upload

---
+++ .github/workflows/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>صفحه اصلی</title>
+</head>
+<body>
+    <h1>به وبسایت ما خوش آمدید!</h1>
+    <p>این وبسایت برای فدراسیون ورزش طراحی شده است.</p>
+    <nav>
+        <ul>
+            <li><a href="index.php">صفحه اصلی</a></li>
+            <li><a href="contact.php">تماس با ما</a></li>
+            <li><a href="register.php">ثبت‌نام</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
